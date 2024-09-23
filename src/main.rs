8e092288b39793f20308ed37015d4671896598d2
ezomo <danielohiozoje@icloud.com>
2024-09-23 14:58:11 +0100

    9/23現在

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,28 @@ use std::time::Duration;
 
 fn main() {
     let pwm_dir_motors = vec![
-        PwmDir::new(17, 18, START_DUTY_CYCLE, FREQUENCY), //　motor id 0
+        PwmDir::new( 17,18, START_DUTY_CYCLE, FREQUENCY),
+        PwmDir::new( 27,22, START_DUTY_CYCLE, FREQUENCY),
+        PwmDir::new( 23,24, START_DUTY_CYCLE, FREQUENCY),
+        PwmDir::new( 25,32, START_DUTY_CYCLE, FREQUENCY),
+        PwmDir::new( 16,26, START_DUTY_CYCLE, FREQUENCY),
+        PwmDir::new( 13,2 , START_DUTY_CYCLE, FREQUENCY),
+
+        PwmDir::new( 3,4 , START_DUTY_CYCLE, FREQUENCY), // 6
+
     ];
 
     let motors = setup_motors(pwm_dir_motors);
 
     let mut udp_communication = motors.clone();
     thread::spawn(move || loop {
-        udp_communication::recv_pwm_udp(&mut udp_communication, "8080");
+        udp_communication::recv_pwm_udp(&mut udp_communication, "60000");
     });
 
     let gpio = Gpio::new().unwrap();
     let limit_switch_pin = [
-        gpio.get(24).unwrap().into_input_pullup(),
-        gpio.get(22).unwrap().into_input_pullup(),
+        gpio.get(8).unwrap().into_input_pullup(),
+        gpio.get(9).unwrap().into_input_pullup(),
     ];
 
     let mut state = 0;
@@ -31,9 +39,9 @@ fn main() {
     loop {
         if limit_switch_pin[0].is_low() {
             // スイッチが押されているか確認
-            *motors[0].lock().unwrap() = 0.;
+            *motors[6].lock().unwrap() = 0.;
             sleep(Duration::from_millis(10));
-            *motors[0].lock().unwrap() = 1.;
+            *motors[6].lock().unwrap() = 1.;
             state = 1;
             state1 = 0;
             println!("0")
@@ -41,10 +49,11 @@ fn main() {
 
         if limit_switch_pin[1].is_low() {
             state1 = 1;
+            println!("2");
         }
         if limit_switch_pin[1].is_high() && state1 >= 1 && state >= 1 {
             println!("{} {}", state1, state);
-            *motors[0].lock().unwrap() = 0.;
+            *motors[6].lock().unwrap() = 0.;
 
             state1 = 0;
             state = 0;
